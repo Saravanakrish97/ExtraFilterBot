17c03a7d152ac3f31a10ea49f652a44541d1cd38
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-01-20 22:04:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,25 @@
-if [ -z $UPSTREAM_REPO ]
+#if [ -z $UPSTREAM_REPO ]
+#then
+#  echo "Cloning main Repository"
+#  git clone https://github.com/Saravanakrish97/ExtraFilterBot.git /ExtraFilterBot 
+#else
+#  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+#  git clone $UPSTREAM_REPO /ExtraFilterBot 
+#fi
+#cd /ExtraFilterBot 
+#pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
+echo "Cloning Repo...."
+if [ -z $BRANCH ]
 then
-  echo "Cloning main Repository"
-  git clone https://github.com/Saravanakrish97/ExtraFilterBot.git /ExtraFilterBot 
+  echo "Cloning main branch...."
+  git clone https://github.com/Saravanakrish97/ExtraFilterBot /ExtraFilterBot
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /ExtraFilterBot 
+  echo "Cloning $BRANCH branch...."
+  git clone https://github.com/Saravanakrish97/ExtraFilterBot -b $BRANCH /ExtraFilterBot
 fi
-cd /ExtraFilterBot 
+cd /ExtraFilterBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
