9bc423c147a589e2edffb52a64271468b9356b29
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-01-21 06:51:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,25 +1,12 @@
-#if [ -z $UPSTREAM_REPO ]
-#then
-#  echo "Cloning main Repository"
-#  git clone https://github.com/Saravanakrish97/ExtraFilterBot.git /ExtraFilterBot 
-#else
-#  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-#  git clone $UPSTREAM_REPO /ExtraFilterBot 
-#fi
-#cd /ExtraFilterBot 
-#pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
-echo "Cloning Repo...."
-if [ -z $BRANCH ]
+if [ -z $UPSTREAM_REPO ]
 then
-  echo "Cloning main branch...."
-  git clone https://github.com/Saravanakrish97/ExtraFilterBot /ExtraFilterBot
+  echo "Cloning main Repository"
+  git clone https://github.com/TeamHMT/Auto-search-tamil-bot.git /Auto-search-tamil-bot
 else
-  echo "Cloning $BRANCH branch...."
-  git clone https://github.com/Saravanakrish97/ExtraFilterBot -b $BRANCH /ExtraFilterBot
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
 fi
-cd /ExtraFilterBot
+cd /Auto-search-tamil-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
